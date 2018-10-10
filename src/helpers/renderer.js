2cc87af981cebd63203a8b2b529990f8908a9374
clapertosa <the_wolf91@libero.it>
2018-10-10 12:09:58 +0200

    Add more html tags

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { renderToString } from "react-dom/server";
-import { StaticRouter } from "react-router-dom";
+import { StaticRouter, Switch } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
 import serialize from "serialize-javascript";
 import routes from "../client/routes";
@@ -10,15 +10,21 @@ const renderer = (path, store, context) => {
   const content = renderToString(
     <Provider store={store}>
       <StaticRouter location={path} context={context}>
-        {renderRoutes(routes)}
+        <Switch>{renderRoutes(routes)}</Switch>
       </StaticRouter>
     </Provider>
   );
 
   return `
   <!DOCTYPE html>
-  <html>
-    <head></head>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Theater - Movies and Series</title>
+      <link rel="stylesheet" href="main.css" />
+    </head>
     <body>
       <div id="root">${content}</div>
       <script>window.INITIAL_STATE = ${serialize(store.getState())}</script>
