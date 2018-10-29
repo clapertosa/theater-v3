761a5d3aa44937e6c453955508803820d0f8d811
clapertosa <the_wolf91@libero.it>
2018-10-29 17:34:40 +0100

    Set baseURL for axios

--- src/client/axiosClientInstance.js
+++ src/client/axiosClientInstance.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 let instance = axios.create({
-  baseURL: `http${process.env.NODE_ENV === "production" ? "s" : ""}://${
-    process.env.HOST
-  }/api || "localhost:3000"/api`
+  baseURL: `http${process.env.NODE_ENV === "production" ? "s" : ""}://${process
+    .env.HOST || "localhost:3000"}/api`
 });
 
 export default instance;
