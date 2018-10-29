fef90a28d59c1b490f75f370e57e837810c63f22
clapertosa <the_wolf91@libero.it>
2018-10-29 18:16:42 +0100

    Set redis secure to false

--- src/client/axiosClientInstance.js
+++ src/client/axiosClientInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 let instance = axios.create({
-  baseURL: `http${process.env.NODE_ENV === "production" ? "s" : ""}://${
-    process.env.NODE_ENV === "production"
-      ? "whispering-earth-57793.herokuapp.com"
-      : "localhost:3000"
+  baseURL: `http${process.env.NODE_ENV === "development" ? "" : "s"}://${
+    process.env.NODE_ENV === "development"
+      ? "localhost:3000"
+      : "whispering-earth-57793.herokuapp.com"
   }/api`
 });
 
