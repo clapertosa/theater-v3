60683a5116a1d3dcfb38d8954fb12c72b2a8583d
clapertosa <the_wolf91@libero.it>
2018-10-29 17:16:24 +0100

    Set baseURL for axios

--- src/client/axiosClientInstance.js
+++ src/client/axiosClientInstance.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 let instance = axios.create({
-  baseURL: `http${process.env.NODE_ENV === "production" ? "s" : ""}://${process
-    .env.host || "localhost:3000"}/api`
+  baseURL: `http${process.env.NODE_ENV === "production" ? "s" : ""}://${
+    process.node.NODE_ENV === "production"
+      ? "whispering-earth-57793.herokuapp.com"
+      : "localhost:3000"
+  }/api`
 });
 
 export default instance;
