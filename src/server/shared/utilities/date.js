641783756b46ff5c10838424af6b974284fa677f
clapertosa <the_wolf91@libero.it>
2018-10-31 15:36:19 +0100

    Better Date handling with Moment.js

--- src/server/shared/utilities/date.js
+++ src/server/shared/utilities/date.js
@@ -1,15 +1,12 @@
-export const getToday = () => {
-  let date = new Date();
+import moment from "moment";
 
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+export const getToday = () => {
+  return moment().format("YYYY-M-D");
 };
 
 export const getOneMonthAgo = () => {
-  let date = new Date();
-
-  return `${
-    date.getMonth() === 0 ? date.getFullYear() - 1 : date.getFullYear()
-  }-${
-    date.getMonth() === 0 ? date.getMonth() + 12 : date.getMonth()
-  }-${date.getDate()}`;
+  return moment()
+    .subtract(30, "days")
+    .format("YYYY-M-D");
 };
+//2018-10-31&primary_release_date.gte=2018-9-31
