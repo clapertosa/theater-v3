c55f1c8caa3e959aa7e9a365c058896d803f3ae9
clapertosa <the_wolf91@libero.it>
2019-04-18 16:15:14 +0200

    Move client Queries in a separate file

--- client/apollo/queries.js
+++ client/apollo/queries.js
@@ -1,4 +1,56 @@
 import { gql } from "apollo-boost";
+
+//* HOME (index)
+export const HOME_MOVIES_QUERY = gql`
+  query HOME_MOVIES_QUERY {
+    homeMovies {
+      results {
+        id
+        original_title
+        original_name
+        title
+        name
+        original_language
+        poster_path
+        backdrop_path
+        adult
+        overview
+        first_air_date
+        release_date
+        genre_ids
+        popularity
+        vote_count
+        vote_average
+      }
+    }
+  }
+`;
+
+export const HOME_SERIES_QUERY = gql`
+  query HOME_SERIES_QUERY {
+    homeSeries {
+      results {
+        id
+        original_title
+        original_name
+        title
+        name
+        original_language
+        poster_path
+        backdrop_path
+        adult
+        overview
+        first_air_date
+        release_date
+        genre_ids
+        popularity
+        vote_count
+        vote_average
+      }
+    }
+  }
+`;
+
 //* SEARCHBAR
 export const SEARCH_QUERY = gql`
   query SEARCH_QUERY($query: String) {
@@ -17,6 +69,42 @@ export const SEARCH_QUERY = gql`
   }
 `;
 
+// DISCOVER
+// Discover Movies
+export const DISCOVER_MOVIES_QUERY = gql`
+  query DISCOVER_MOVIES_QUERY(
+    $page: Int
+    $release_year: String
+    $sort_by: String
+    $with_genres: String
+    $with_cast: String
+  ) {
+    discoverMovies(
+      input: {
+        page: $page
+        release_year: $release_year
+        sort_by: $sort_by
+        with_genres: $with_genres
+        with_cast: $with_cast
+      }
+    ) {
+      page
+      total_pages
+      total_results
+      results {
+        id
+        title
+        original_title
+        release_date
+        vote_average
+        overview
+        poster_path
+        media_type
+      }
+    }
+  }
+`;
+
 export const SEARCH_CAST_QUERY = gql`
   query SEARCH_CAST_QUERY($query: String) {
     searchCast(query: $query) {
@@ -24,6 +112,41 @@ export const SEARCH_CAST_QUERY = gql`
         id
         name
         profile_path
+      }
+    }
+  }
+`;
+
+// Discover Series
+export const DISCOVER_SERIES_QUERY = gql`
+  query DISCOVER_SERIES_QUERY(
+    $page: Int
+    $release_year: String
+    $sort_by: String
+    $with_genres: String
+    $without_genres: String
+  ) {
+    discoverSeries(
+      input: {
+        page: $page
+        release_year: $release_year
+        sort_by: $sort_by
+        with_genres: $with_genres
+        without_genres: $without_genres
+      }
+    ) {
+      page
+      total_pages
+      total_results
+      results {
+        id
+        name
+        original_name
+        first_air_date
+        vote_average
+        overview
+        poster_path
+        media_type
       }
     }
   }
