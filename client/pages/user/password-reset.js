906e9519222d742fd01bf3333c6a7a38ade7d49b
clapertosa <the_wolf91@libero.it>
2019-05-07 15:57:45 +0200

    Password-reset page accessible only if a token is provided

--- client/pages/user/password-reset.js
+++ client/pages/user/password-reset.js
@@ -1,11 +1,15 @@
+import redirect from "../../lib/redirect";
 import Form from "../../components/Form/PasswordReset/Form";
 
 const PasswordReset = ({ token }) => {
   return <Form token={token} />;
 };
 
-PasswordReset.getInitialProps = async ({ query }) => {
-  return { token: query.token };
+PasswordReset.getInitialProps = async ctx => {
+  if (!ctx.query.token) {
+    redirect(ctx, "/");
+  }
+  return { token: ctx.query.token };
 };
 
 export default PasswordReset;
