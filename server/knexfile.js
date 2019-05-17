0263d1570d2f982b693a6bacfeaaa69622b52f81
clapertosa <the_wolf91@libero.it>
2019-05-17 18:07:55 +0200

    Set URL connection for postgres in production

--- server/knexfile.js
+++ server/knexfile.js
@@ -21,11 +21,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: {
-      database: "theater",
-      user: "username",
-      password: "password"
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
