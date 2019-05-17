b1afec985cdb5027a7570db84570fa0c419203fb
clapertosa <the_wolf91@libero.it>
2019-05-17 12:47:59 +0200

    Update queries in sign-out page

--- client/pages/user/signout.js
+++ client/pages/user/signout.js
@@ -1,6 +1,10 @@
 import redirect from "../../lib/redirect";
 import { SIGNOUT_MUTATION } from "../../apollo/mutations";
-import { CURRENT_USER_QUERY, GET_FAVORITES_QUERY } from "../../apollo/queries";
+import {
+  CURRENT_USER_QUERY,
+  GET_FAVORITES_MOVIES_QUERY,
+  GET_FAVORITES_SERIES_QUERY
+} from "../../apollo/queries";
 
 const SignOut = () => {
   return null;
@@ -14,7 +18,8 @@ SignOut.getInitialProps = async ctx => {
       query: CURRENT_USER_QUERY,
       data: { currentUser: null }
     },
-    { query: GET_FAVORITES_QUERY, data: { getFavorites: null } }
+    { query: GET_FAVORITES_MOVIES_QUERY, data: { getFavoritesMovies: null } },
+    { query: GET_FAVORITES_SERIES_QUERY, data: { getFavoritesSeries: null } }
   );
 
   redirect(ctx, "/");
